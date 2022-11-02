a7028e40cb2119455988bcebfbc4cbb3c9a31116
Henry Huntington <henryxhuntington@gmail.com>
2022-11-02 14:21:10 -0700

    finished js functionality

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ headDropdown.addEventListener('change', () => {
     // increment the head change count state
     headCount++;
     // update the dom for the head (use style.backgroundImage on the headEl div instead of trying to set the .src -- it's NOT an img tag!)
-    headEl.style.backgroundImage = `url("./assets/${value}-head.png")`;
+    headEl.style.backgroundImage = `url('./assets/${value}-head.png')`;
     // update the stats to show the new count (call displayStats() to do this work)
     displayStats();
 });
@@ -35,7 +35,7 @@ middleDropdown.addEventListener('change', () => {
     // increment the middle change count state
     middleCount++;
     // update the dom for the middle (NOTE: use style.backgroundImage on the middleEl div instead of trying to set the .src -- it's NOT an img tag!)
-    middleEl.style.backgroundImage; = `url("./assets/${value}-middle.png")`;
+    middleEl.style.backgroundImage = `url('./assets/${value}-middle.png')`;
     // update the stats to show the new count (call displayStats() to do this work)
     displayStats();
 });
@@ -46,6 +46,7 @@ bottomDropdown.addEventListener('change', () => {
     // increment the bottom change count state
     bottomCount++;
     // update the dom for the bottom (NOTE use style.backgroundImage on the bottomEl div instead of trying to set the .src -- it's NOT an img tag!)
+    bottomEl.style.backgroundImage = `url('./assets/${value}-pants.png')`;
     // update the stats to show the new count (call displayStats() to do this work)
     displayStats();
 });
@@ -63,9 +64,9 @@ catchphraseButton.addEventListener('click', () => {
 
 function displayStats() {
     // text content of the reportEl to tell the user how many times they've changed each piece of the state
-    const statsString = creatStatsString(headCount, middleCount, bottomCount);
+    // const statsString = creatStatsString(headCount, middleCount, bottomCount);
 
-    reportEl.textContent = statsString;
+    reportEl.textContent = `You have changed the head ${headCount} times, the body ${middleCount} times, and the legs ${bottomCount} times.`;
 }
 
 function displayCatchphrases() {
